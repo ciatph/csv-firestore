e80a3e2e64dbae5b59172719b7bd2322b3d6b618
ciatph <ciat.ph@gmail.com>
2022-03-23 01:49:37 +0800

    Check required env variables.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
+if (!process.env.FIREBASE_SERVICE_ACC || !process.env.FIREBASE_PRIVATE_KEY) {
+  console.log('FIREBASE_SERVICE_ACC or FIREBASE_PRIVATE_KEY is missing.')
+  process.exit(1)
+}
+
 const CsvToFirestore = require('./src/lib/classes/csvtofirestore')
 
 module.exports = CsvToFirestore
